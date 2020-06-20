d59c609fe327c2ed0fb4be5940a2406ddd9ec17e
Gradson <gmesquita@atech.com.br>
2020-06-19 23:05:35 -0300

    curso concluído

---
+++ controles/desafioswitch/desafio.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func notaParaConceito(n float64) string {
+	switch {
+	case n >= 9 && n <= 10:
+		return "A"
+	case n >= 8:
+		return "B"
+	case n >= 5:
+		return "C"
+	case n >= 3:
+		return "D"
+	default:
+		return "E"
+	}
+}
+
+func main() {
+	fmt.Println(notaParaConceito(9.8))
+	fmt.Println(notaParaConceito(6.9))
+	fmt.Println(notaParaConceito(2.1))
+}
